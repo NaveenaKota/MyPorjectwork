5fe1c0744826653e90f9c5f1a720ac0607654784
NaveenaKota <kotan1@newpaltz.edu>
2022-03-11 22:54:01 -0500

    Added response for issue

--- client/src/pages/Issue.vue
+++ client/src/pages/Issue.vue
@@ -2,15 +2,17 @@
 import { ref } from 'vue'
 const clientInfo = ref('');
 const email = ref('');
+const response = ref('')
 function reset(){
     clientInfo.value = '';
     email.value = ''
+    response.value = 'Sorry for the trouble caused. It will be addressed shortly.'
 }
 </script>
 
 <template>
-    <div class="container columns is-centered">
-        <form class="box">
+    <div class="field">
+        <form @submit.prevent="reset" class="box" >
         <div class="column is-two-thirds">
             <h1 class="title">Report a bug</h1>
             <p>We apologise for the inconvenience. Please describe the issue you are facing and we will get back to you with a fix.</p>
@@ -28,11 +30,14 @@ function reset(){
 			<input type="hidden" id="javascript-disabled" name="javascript-disabled" value="true">
 		</noscript>
         
-		    <button type="submit" @click="reset">Submit Report</button>
+		    <button type="submit" >Submit Report</button>
        
         </div>
+        
         </form>
+        <h1 >{{response}}</h1>
     </div>
+    
 </template>
 
 <style scoped>
