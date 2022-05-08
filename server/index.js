cac0ed772626d021d42fcfdcc384519c38cb047a
NaveenaKota <kotan1@newpaltz.edu>
2022-05-08 22:07:40 +0500

    server init

--- server/index.js
+++ server/index.js
@@ -1,24 +1,47 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+
+const userModel = require('./models/user');
+const usersController = require('./controllers/users');
+const postsController = require('./controllers/posts');
+
+const { requireAuth } = require('./models/auth');
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app
-.get('/', (req, res) => {
-  res.send('You are on the homepage')
-})
-.get('/about', (req, res) => {
-  res.send('You are on the about page')
-})
-.get('/contact', (req, res) => {
-  res.send({
-    email: 'kotan1@newpaltz.edu',
-    phone: '845-375-6253',
-    instagram:'naveena'
-  })
-})
+	.use(function(req, res, next) {
+			res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+			res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+			next();
+	})
+	.use(express.json())
+	.use((req, res, next) => {
+			const auth = req.headers.authorization;
+			if(auth){
+					const token = auth.split(' ')[1];
+					userModel.fromToken(token)
+					.then(user => {
+							req.user = user;
+							next();
+					}).catch(next);
+			}else{
+					next();
+			}
+	})
+	.get('/api/', (req, res) => {
+			res.send('You are at the root of the API.');
+	})
+	.use('/api/users', usersController)
+	.use('/api/posts', /*requireAuth,*/ postsController)
+	.use((err, req, res, next) => {
+			console.error(err);
+			res .status(err.statusCode || 500)
+					.send({ errors: [ err.message ?? 'Internal server error' ] });
+	})
+	.listen(port, () => {
+		console.log(`Listening @ http://localhost:${port}`)
+	})
 
 
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
